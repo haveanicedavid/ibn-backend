5afc3a31ae8105731eec2c96c41544dafcc0a50f
David Daniel <davedan7@gmail.com>
2017-03-19 13:20:16 -0600

    squish me

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 import express from 'express'
 import axios from 'axios'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 
 const app = express()
 const port = process.env.port || 8080
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
   axios.get('https://btc-e.com/api/3/ticker/ltc_btc')
     .then((response) => {
       console.log(response)
       res.send(response.data)
-      // res.send('working')
     })
     .catch((error) => {
       console.log(error)
-      res.send('fuck')
     })
 })
 
