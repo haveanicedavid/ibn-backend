856fcf758fa0205a2504076d239e4953326b57ca
David Daniel <davedan7@gmail.com>
2017-03-19 19:55:28 -0600

    Fixed permissions error. Basic mongo setup

---
+++ README.md
@@ -0,0 +1,3 @@
+### Setup
+1. Make sure you have [Yarn](https://yarnpkg.com) and [Mongodb](https://www.mongodb.com) installed locally
+2. Make sure your mongo server is running (`mongod`)
--- server.js
+++ server.js
@@ -2,24 +2,49 @@ import express from 'express'
 import axios from 'axios'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose, { Schema } from 'mongoose'
 
 const app = express()
-const port = process.env.port || 8080
+
+mongoose.connect('mongodb://localhost/exchange-snaps')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors())
 
+// TODO move this out to models
+const SnapSchema = new Schema({
+  ltcRate: {
+    type: Number
+  }
+  // created: {
+  //   type: Date
+  // }
+})
+
+const Snap = mongoose.model('Snap', SnapSchema)
+
 app.get('/', (req, res) => {
   axios.get('https://btc-e.com/api/3/ticker/ltc_btc')
     .then((response) => {
-      console.log(response)
-      res.send(response.data)
+      const rate = response.data.ltc_btc.buy // using BTC to buy ETH
+      const ltcSnap = new Snap({ ltcRate: rate })
+      console.log(rate)
+      ltcSnap.save()
+      res.send(JSON.stringify(rate))
     })
     .catch((error) => {
       console.log(error)
     })
 })
+
+app.get('/snaps', (req, res) => {
+  Snap.find({}, (err, snaps) => {
+    res.send(snaps)
+  })
+})
+
+const port = process.env.port || 8080
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
