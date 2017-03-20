330f1c6a77cc7ea071cc387b63de1be7e634af53
David Daniel <davedan7@gmail.com>
2017-03-20 16:11:45 -0600

    Shore up tests

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ### Setup
 1. Make sure you have [Yarn](https://yarnpkg.com) and [Mongodb](https://www.mongodb.com) installed locally
 2. Make sure your mongo server is running (`mongod`)
+
+## Notes
+1. I built this using babel-node to use ES6. This is great for development and testing, but apparently is very slow on production. If I were to start this challenge over, I would use ES25 syntax (should mainly just be changing ES6 `import` statements to commonJS `require()`)
--- test/server.spec.js
+++ test/server.spec.js
@@ -1,30 +1,82 @@
 /* global it, describe */
 import app from '../server/server'
-import { assert } from 'chai'
+import Snap from '../server/api/snap/snapModel'
+import { assert, expect } from 'chai'
 import request from 'supertest'
+import sinon from 'sinon'
+import 'sinon-mongoose'
 
-describe('Server', function () {
-  it('should start accept get requests', () => {
-    const arr = []
-    assert.equal(arr.length, 0)
-  })
-  it('respond with json', function (done) {
+describe('Server', () => {
+  // Test will pass if server responds with JSON
+  it('should GET all snaps', (done) => {
     request(app)
-      .get('/snaps')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, done)
-  })
-  it('should GET all snaps', function (done) {
-    request(app)
-      .get('/snaps')
+      .get('/api/snaps')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200)
-      .end(function (err, resp) {
+      .end((err, resp) => {
         if (err) console.log(err)
         assert.isOk(resp)
         done()
       })
+  })
+})
+
+describe('GET /api/snaps', (done) => {
+  // Test will pass if we get all snaps
+  it('should return all snaps', (done) => {
+    const SnapMock = sinon.mock(Snap)
+    const expectedResult = { status: true, snap: [] }
+    SnapMock.expects('find').yields(null, expectedResult)
+    Snap.find((err, result) => {
+      if (err) console.log(err)
+      SnapMock.verify()
+      SnapMock.restore()
+      expect(result.status).to.be.true
+      done()
+    })
+  })
+
+  // Test will pass if we fail to get a Snap
+  it('should return error', (done) => {
+    const SnapMock = sinon.mock(Snap)
+    const expectedResult = { status: false, error: 'Something went wrong' }
+    SnapMock.expects('find').yields(expectedResult, null)
+    Snap.find((err, result) => {
+      SnapMock.verify()
+      SnapMock.restore()
+      expect(err.status).to.not.be.true
+      done()
+    })
+  })
+})
+
+describe('Create a new Snap', () => {
+  it('should create new post', (done) => {
+    var SnapMock = sinon.mock(new Snap({ Snap: 'Save new Snap from mock' }))
+    var snap = SnapMock.object
+    var expectedResult = { status: true }
+    SnapMock.expects('save').yields(null, expectedResult)
+    snap.save((err, result) => {
+      if (err) console.log(err)
+      SnapMock.verify()
+      SnapMock.restore()
+      expect(result.status).to.be.true
+      done()
+    })
+  })
+  // Test will pass if the Snap is not saved
+  it('should return error, if post not saved', (done) => {
+    var SnapMock = sinon.mock(new Snap({ Snap: 'Save new Snap from mock' }))
+    var snap = SnapMock.object
+    var expectedResult = { status: false }
+    SnapMock.expects('save').yields(expectedResult, null)
+    snap.save(function (err, result) {
+      if (err) console.log(err)
+      SnapMock.verify()
+      SnapMock.restore()
+      expect(err.status).to.not.be.true
+      done()
+    })
   })
 })
